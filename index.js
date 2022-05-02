0c7a484c2bdff5fb3f72f8a6796d5ff5f8ea81f1
Matt <i.w.b.s.bilm@gmail.com>
2022-05-01 19:54:30 -0700

    server built

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-
+app.get('/', (req, res) => {res.send(`<h1>Hello</h1>`)})
 app.listen('9000',()=>{console.log('listening on port 9000')})
